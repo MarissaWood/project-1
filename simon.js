2695451650d9b4ed06bd84c3297c04e2a014432b
Marissa Wood <marissa.espina@gmail.com>
2018-05-30 10:45:25 -0400

    created a function to play the level pattern

--- simon.js
+++ simon.js
@@ -1,41 +1,100 @@
-//variables for the game
+// variables for the game
 
-let level=1 
-let playerPattern=[]
+let level = 1
+let playerPattern = []
+let i = 0
 
-//add event listeners for each button
-
+// add event listeners for each button
 let green = document.querySelector('#green')
 green.addEventListener('click', greenClick)
 green.addEventListener('click', lightUp)
 
-/*
 let red = document.querySelector('#red')
 red.addEventListener('click', redClick)
+red.addEventListener('click', lightUp)
 
 let yellow = document.querySelector('#yellow')
 yellow.addEventListener('click', yellowClick)
+yellow.addEventListener('click', lightUp)
 
 let blue = document.querySelector('#blue')
 blue.addEventListener('click', blueClick)
-*/
-//create functions for when each button is pushed
+blue.addEventListener('click', lightUp)
 
+// create functions for when each button is pushed
 function lightUp (e) {
-    this.classList.add("clicked")
-    setTimeout (function () {
-        e.target.classList.remove("clicked")}, 100)
+  this.classList.add('clicked')
+  setTimeout(function () {
+    e.target.classList.remove('clicked')
+  }, 100)
 }
 
 function greenClick () {
-    playerPattern.push("g")
-    console.log("green clicked")
+  playerPattern.push('G')
 }
 
+function redClick () {
+  playerPattern.push('R')
+}
+
+function yellowClick () {
+  playerPattern.push('Y')
+}
+
+function blueClick () {
+  playerPattern.push('B')
+}
+
+let levelPattern = ['B', 'Y', 'R', 'G']
+
+function lightUpPlay () {
+  // light up the correct tile
+  if (levelPattern[i] === 'R') {
+    setTimeout(function () {
+        red.classList.add('clicked')}, (i*1000+100))
+    setTimeout(function () {
+      red.classList.remove('clicked')
+    }, (i+1)*1000)
+  }// closes red
+
+  if (levelPattern[i] === 'G') {
+    setTimeout(function () {
+        green.classList.add('clicked')}, (i*1000+100))
+    setTimeout(function () {
+      green.classList.remove('clicked')
+    }, (i+1)*1000)
+  }// closes green
+
+  if (levelPattern[i] === 'Y') {
+    setTimeout(function () {
+        yellow.classList.add('clicked')}, (i*1000+100))
+    setTimeout(function () {
+      yellow.classList.remove('clicked')
+    }, (i+1)*1000)
+  }// closes yellow
+
+  if (levelPattern[i] === 'B') {
+    setTimeout(function () {
+        blue.classList.add('clicked')}, (i*1000+100))
+    setTimeout(function () {
+      blue.classList.remove('clicked')
+    }, (i+1)*1000)
+  }// closes blue
+
+}// closes lightUpPlay
+
+function playLevel () {
+  lightUpPlay()
+  //pause between buttons???
+  if (i < levelPattern.length) {
+    i++
+  playLevel()
+  }
+}// closes playLevel
+
+// playLevel()
+
 /*
-//play level pattern
-
-
 //get player input pattern
     //check if input is correct, advance level
     if (playerPattern===levelPattern) {
@@ -48,8 +107,6 @@ patterns = {
     2: []
 }
 */
-//random patterns
+// random patterns
 
-
-
-//initiate game
+// initiate game
