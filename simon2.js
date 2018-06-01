f378fc4f5dea11cd3412cd5e643d3ab9bb38bd34
Marissa Wood <marissa.espina@gmail.com>
2018-06-01 10:22:16 -0400

    worked on the second javascript file to turn each button into an object, still work in progress

--- simon2.js
+++ simon2.js
@@ -1,27 +1,45 @@
+// this is my attempt at making my code DRY-er (very much a work in progress)
+
 class Button {
-    constructor (object, color, sound,) {
-        this.object = object
-        this.color = color
-        this.sound = sound
+  constructor (object, color, sound) {
+    this.object = object
+    this.color = color
+    this.sound = sound
+  }
 
-        this.playSound () {
-            this.sound.play()
-        }
-        this.lightUp (e) {
-            this.object.classList.add('clicked')
-            setTimeout(function () {
-              e.target.classList.remove('clicked')
-            }, 100)
-        }
+  playSound () {
+    this.sound.play()
+  }
 
-        this.clicked () {
-            this.object.addEventListener('click', click)
-        }
-    }
+  lightUp (e) {
+    e.target.classList.add('clicked')
+    setTimeout(function () {
+      e.target.classList.remove('clicked')
+    }, 100)
+  }
+
+  clicked (e) {
+    playerPattern.push(this.color)
+    console.log(this.color)
+    //this.playSound()
+    //this.lightUp(e)
+  }
 }
 
-function click() {
+let level = 1
+let playerPattern = []
+let levelPattern = ['G', 'B', 'R', 'R'] // default starting pattern
+let i = 0 // variable for playing colors first
+let j = 0 // variable for keeping track of player input
+let n = 4 // variable for number of notes
+let ascension = false
 
-}
+const red = new Button(document.querySelector('#red'), 'R', document.querySelectorAll('audio')[0])
+const green = new Button(document.querySelector('#green'), 'G', document.querySelectorAll('audio')[1])
+const yellow = new Button(document.querySelector('#yellow'), 'Y', document.querySelectorAll('audio')[2])
+const blue = new Button(document.querySelector('#blue'), 'B', document.querySelectorAll('audio')[3])
 
-const red = new Button(document.querySelector('#red'), "R", document.querySelectorAll('audio')[0])
+red.object.addEventListener('click', red.clicked)
+green.object.addEventListener('click', green.clicked)
+yellow.object.addEventListener('click', yellow.clicked)
+blue.object.addEventListener('click', blue.clicked)
